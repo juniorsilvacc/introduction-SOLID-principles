c5b428263c3e55c09004917f2bb6df9e5dd774dd
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-08-30 00:37:49 -0300

    test: ensure not to delete user if it does not exist

---
+++ src/modules/users/useCases/DeleteUser/delete-user-usecase.spec.ts
@@ -0,0 +1,21 @@
+import { AppError } from '../../../../config/errors/app-error';
+import { InMemoryUsersRepository } from '../../repositories/in-memory/in-memory-users-repository';
+import { DeleteUserUseCase } from './delete-user-usecase';
+
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let deleteUserUseCase: DeleteUserUseCase;
+
+describe('Delete User', () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    deleteUserUseCase = new DeleteUserUseCase(inMemoryUsersRepository);
+  });
+
+  test('should not be able to delete a user does not exist', async () => {
+    expect(
+      deleteUserUseCase.execute({
+        id: 'non-exists',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
